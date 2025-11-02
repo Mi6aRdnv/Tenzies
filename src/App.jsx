bda02b536c6acf95ca0cb98409f47dbda80f5e12
Mi6aRdnv <95938371+Mi6aRdnv@users.noreply.github.com>
2025-11-02 18:14:32 +0100

    feat: add Die component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import Die from "../src/Die.jsx";
+
 function App() {
-	return <main className="main"></main>;
+	const randomDice = Array.from({ length: 10 }, () =>
+		Math.ceil(Math.random() * 6)
+	);
+
+	return (
+		<main className="main">
+			<div className="dice-container">
+				<Die value={randomDice[0]} />
+				<Die value={randomDice[1]} />
+				<Die value={randomDice[2]} />
+				<Die value={randomDice[3]} />
+				<Die value={randomDice[4]} />
+				<Die value={randomDice[5]} />
+				<Die value={randomDice[6]} />
+				<Die value={randomDice[7]} />
+				<Die value={randomDice[8]} />
+				<Die value={randomDice[9]} />
+			</div>
+		</main>
+	);
 }
 
 export default App;
