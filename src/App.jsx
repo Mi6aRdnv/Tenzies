dc242ff59085d4c93186f770218187e61d4c3fff
Mi6aRdnv <95938371+Mi6aRdnv@users.noreply.github.com>
2025-11-02 19:00:16 +0100

    feat: add Die holding functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,43 @@ function App() {
 		return Array.from({ length: 10 }, () => ({
 			value: Math.ceil(Math.random() * 6),
 			id: crypto.randomUUID(),
+			isHeld: false,
 		}));
+	}
+	function hold(id) {
+		setDice((oldDice) =>
+			oldDice.map((die) => {
+				return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+			})
+		);
+	}
+	function rollUnheldDice() {
+		setDice((oldDice) =>
+			oldDice.map((die) => {
+				return !die?.isHeld
+					? {
+							...die,
+							value: Math.ceil(Math.random() * 6),
+						}
+					: die;
+			})
+		);
 	}
 	const [dice, setDice] = useState(() => setStartDice());
 
 	const diceElements = dice.map((die) => (
-		<Die key={die.id} value={die.value} />
+		<Die
+			key={die.id}
+			value={die.value}
+			isHeld={die.isHeld}
+			hold={() => hold(die.id)}
+		/>
 	));
 
 	return (
 		<main className="main">
 			<div className="dice-container">{diceElements}</div>
-			<button onClick={() => setDice(setStartDice())} className="roll-dice">
+			<button onClick={() => rollUnheldDice()} className="roll-dice">
 				Würfeln
 			</button>
 		</main>
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,3 +1,12 @@
-export default function Die({ value }) {
-	return <button className="die">{value}</button>;
+export default function Die({ value, isHeld, hold }) {
+	return (
+		<button
+			onClick={hold}
+			className={`${isHeld && "die--held"} die`}
+			aria-pressed={isHeld}
+			aria-label={`${isHeld ? "Gehaltener" : "Nicht gehaltener"} Würfel mit dem Wert ${value}`}
+		>
+			{value}
+		</button>
+	);
 }
